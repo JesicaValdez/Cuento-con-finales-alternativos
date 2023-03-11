d89b1034de5b245c79faaae6a628bce8fc3e0373
JesicaValdez <jesica.valddez@gmail.com>
2023-03-11 16:45:27 -0300

    segundo commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let final = document.getElementById ("final");
+let Dame = document.getElementById ("Dame");
+let Dame2= document.getElementById ("Dame2");
+let Dame3= document.getElementById ("Dame3");
+
+Dame.addEventListener("click", () => {
+    final.innerHTML = "El niño se hizo un joven casi adulto y quiso llenar su vacío interior adentrándose por las intransitables selvas de la droga, alcohol y la delincuencia. Cada vez se encontró mayor rechazo social y familiar, y tras ingresar en centros de menores , y posteriormente, penales, al final, murió joven, en una espiral de violencia y furia, llevándose por delante a cuantos inocentes osaron enfrentarse en su camino de violencia y destrucción.";
+});
+
+Dame2.addEventListener("click", () => {
+    final.innerHTML = "Siendo consciente de la imposibilidad de adaptarse al mundo que le rodeaba – o rechazaba, mas bien-, el niño – adolescente ya – en un descuido de sus padres, se suicidó para mejor culpar al mundo del dolor que su herida primigenia le causaba, dejando un reguero de dolor sordo perpetuo en su familia."; 
+});    
+
+Dame3.addEventListener("click", () => {
+    final.innerHTML = "Aún con muchas dificultades, la familia, y el colegio mantuvieron un esfuerzo coordinado por hacerle sentir especialmente amado y bueno. Pudo tener, por fin, un amigo, haciéndolo sentir, por primera vez, merecedor del afecto de sus iguales. Un profesor se atrevió a mirar mas allá de su apariencia hosca, y logró hacerse un hueco en su corazón atribulado. Inconmensurables cantidades de amor y paciencia de todos cuantos le rodearon le ayudaron a llenar su vacío interior y restañar su herida primigenia con el amor de sus semejantes. Y cuando alcanzó la madurez, pudo elevarse más allá de su doloroso origen, y desarrollar una vida plena, criar una familia, y crear relaciones plenas afectivas con cuantos lo rodearon, y hacer felices a sus semejantes siendo feliz el mismo.";
+})
